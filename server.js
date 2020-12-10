5a856c577990950a10d7971d7a00789251bddfe8
masatake-azumi <190251lp@yse-c.net>
2020-12-10 12:41:59 +0900

    １２月１０日変更

--- server.js
+++ server.js
@@ -1,22 +1,62 @@
-//express モジュールの読み込み
+//モジュール 読み込み
 const express = require('express');
+
 
 //サーバ作成
 const app = express();
 
-//ルーティング
-//Webルートにリクエストされたらレスポンス
-app.get('/',(req,res) =>{
-    res.setDefaultEncoding('Hello Express!');
+// URLエンコードされたデータを解析する
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+
+
+// ミドルウェア関数
+// 全てのリクエスト
+app.use((req, res, next) => {
+    console.log(`middleware: all. url: ${req.url}`);
+
+    //CROS設定: 全てのドメインに対して許可 
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+    //次の処理
+    next();
 });
 
-//app.get(URLパス、処理);
 
-//無名関数の書き方
-//app.get('/',function(req,res){
+// Webルートリクエスト(POST)
+app.post("/", (req, res) => {
+    //GET パラメータ取得 (id)
+    let id = req.query.id;
+    console.log(id);
 
-//});
+    //POST データ取得 (message)
+    console.log(req.body);
 
-app.listen(3000);
+    let message = req.body.message;
+    console.log(message);
 
+    //日時生成
+    let datetime = new Date();
+
+    let result = {
+        'id': id,
+        'message': message,
+        'datetime': datetime
+    };
+
+    //レスポンス
+    res.send(result);
+});
+
+app.get("/",(req,res) => {
+    res.send("Hello Express!!!");
+})
+
+
+
+
+//サーバ待機
+app.listen (3000);
+    console.log("Server listen:http://localhost:3000");
